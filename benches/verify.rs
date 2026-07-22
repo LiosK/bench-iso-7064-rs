3a55839a128e6f118b6c8b4d8a49b2a0652b38f5
LiosK <contact@mail.liosk.net>
2026-07-21 20:44:59 -0400

    Group charset-related operations as modules

--- benches/verify.rs
+++ benches/verify.rs
@@ -23,11 +23,22 @@ fn gen_samples(charset: &[u8]) -> Vec<String> {
     xs
 }
 
-static NUMERIC: LazyLock<Vec<String>> = LazyLock::new(|| gen_samples(b"0123456789"));
-static ALPHABETIC: LazyLock<Vec<String>> =
-    LazyLock::new(|| gen_samples(b"ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-static ALPHANUMERIC: LazyLock<Vec<String>> =
-    LazyLock::new(|| gen_samples(b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+mod numeric {
+    use super::*;
+    pub static SAMPLES: LazyLock<Vec<String>> = LazyLock::new(|| gen_samples(b"0123456789"));
+}
+
+mod alphabetic {
+    use super::*;
+    pub static SAMPLES: LazyLock<Vec<String>> =
+        LazyLock::new(|| gen_samples(b"ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+}
+
+mod alphanumeric {
+    use super::*;
+    pub static SAMPLES: LazyLock<Vec<String>> =
+        LazyLock::new(|| gen_samples(b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+}
 
 macro_rules! gen_benches {
     ($system:ident, $charset:ident, $type_x:ident, $type_y:ident) => {
@@ -37,8 +48,8 @@ macro_rules! gen_benches {
 
             #[test]
             fn compare_results() {
-                let list = &*$charset;
-                for s in list {
+                let samples = &*$charset::SAMPLES;
+                for s in samples {
                     let x = iso_7064::$type_x.verify(s).unwrap_or(false);
                     let y = iso_iec_7064::$type_y.validate_string(s);
                     assert_eq!(x, y);
@@ -47,9 +58,9 @@ macro_rules! gen_benches {
 
             #[bench]
             fn bench_iso_7064(b: &mut test::Bencher) {
-                let list = &*$charset;
+                let samples = &*$charset::SAMPLES;
                 b.iter(|| {
-                    list.iter().fold(false, |acc, s| {
+                    samples.iter().fold(false, |acc, s| {
                         acc ^ iso_7064::$type_x.verify(s).unwrap_or(false)
                     })
                 });
@@ -57,9 +68,9 @@ macro_rules! gen_benches {
 
             #[bench]
             fn bench_iso_7064_over_bytes(b: &mut test::Bencher) {
-                let list = &*$charset;
+                let samples = &*$charset::SAMPLES;
                 b.iter(|| {
-                    list.iter().fold(false, |acc, s| {
+                    samples.iter().fold(false, |acc, s| {
                         acc ^ iso_7064::$type_x
                             .verify_from_chars(s.bytes().map(char::from))
                             .unwrap_or(false)
@@ -69,9 +80,9 @@ macro_rules! gen_benches {
 
             #[bench]
             fn bench_iso_iec_7064(b: &mut test::Bencher) {
-                let list = &*$charset;
+                let samples = &*$charset::SAMPLES;
                 b.iter(|| {
-                    list.iter().fold(false, |acc, s| {
+                    samples.iter().fold(false, |acc, s| {
                         acc ^ iso_iec_7064::$type_y.validate_string(s)
                     })
                 });
@@ -80,11 +91,11 @@ macro_rules! gen_benches {
     };
 }
 
-gen_benches!(pure1_mod11_2, NUMERIC, MOD11_2, MOD_11_2);
-gen_benches!(pure1_mod37_2, ALPHANUMERIC, MOD37_2, MOD_37_2);
-gen_benches!(pure2_mod97_10, NUMERIC, MOD97_10, MOD_97_10);
-gen_benches!(pure2_mod661_26, ALPHABETIC, MOD661_26, MOD_661_26);
-gen_benches!(pure2_mod1271_36, ALPHANUMERIC, MOD1271_36, MOD_1271_36);
-gen_benches!(hybrid_mod11_10, NUMERIC, MOD11_10, MOD_11_10);
-gen_benches!(hybrid_mod27_26, ALPHABETIC, MOD27_26, MOD_27_26);
-gen_benches!(hybrid_mod37_36, ALPHANUMERIC, MOD37_36, MOD_37_36);
+gen_benches!(pure1_mod11_2, numeric, MOD11_2, MOD_11_2);
+gen_benches!(pure1_mod37_2, alphanumeric, MOD37_2, MOD_37_2);
+gen_benches!(pure2_mod97_10, numeric, MOD97_10, MOD_97_10);
+gen_benches!(pure2_mod661_26, alphabetic, MOD661_26, MOD_661_26);
+gen_benches!(pure2_mod1271_36, alphanumeric, MOD1271_36, MOD_1271_36);
+gen_benches!(hybrid_mod11_10, numeric, MOD11_10, MOD_11_10);
+gen_benches!(hybrid_mod27_26, alphabetic, MOD27_26, MOD_27_26);
+gen_benches!(hybrid_mod37_36, alphanumeric, MOD37_36, MOD_37_36);
